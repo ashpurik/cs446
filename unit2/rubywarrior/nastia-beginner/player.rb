e4e44b8e1cf978a68876520574dc5c6e38b1c376
Anastasia Shpurik <ashpurik@mines.edu>
2014-01-20 17:04:39 -0700

    level 4 complete

--- unit2/rubywarrior/nastia-beginner/player.rb
+++ unit2/rubywarrior/nastia-beginner/player.rb
@@ -1,8 +1,21 @@
 class Player
+
+#Set @health to your current health at the end of the turn. If this is greater
+#than your current health next turn then you know you're taking damage and
+#shouldn't rest.
+
+
+  @health = nil
+
   def play_turn(warrior)
 
+    if @health == nil
+      @health = warrior.health
+    end
+
     if warrior.feel.empty?
-      if warrior.health < 10
+
+      if warrior.health >= @health && warrior.health < 12
         warrior.rest!
       else
         warrior.walk!
@@ -11,6 +24,8 @@ class Player
     else
       warrior.attack!
     end
+
+    @health = warrior.health
 
   end
 end
