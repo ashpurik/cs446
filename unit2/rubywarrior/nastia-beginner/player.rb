6741c02fe05840a13e04f992cb92a2b5c046f045
Anastasia Shpurik <ashpurik@mines.edu>
2014-01-20 21:04:00 -0700

    completed level 7

--- unit2/rubywarrior/nastia-beginner/player.rb
+++ unit2/rubywarrior/nastia-beginner/player.rb
@@ -1,14 +1,36 @@
 class Player
 
   @health = nil
+  MINIMUM = 8
+  MAXIMUM = 20
+  @saved = nil
+
 
   def play_turn(warrior)
 
     @health = warrior.health unless @health != nil
+    @saved = false unless @saved != nil
 
-    if warrior.feel.empty?
+    if warrior.feel.wall?
+      warrior.pivot! :backward
+      @saved = true
 
-      if warrior.health >= @health && warrior.health < 13
+    #BACKWARD:
+    elsif warrior.feel(:backward).empty? && @saved == false
+      warrior.walk! :backward
+
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue! :backward
+      @saved = true
+        
+    #FORWARD
+    elsif warrior.feel.empty?
+
+      if warrior.health < @health && warrior.health <= MINIMUM
+        warrior.walk! :backward
+      elsif warrior.health < @health && warrior.health > MINIMUM
+        warrior.walk!
+      elsif warrior.health >= @health && warrior.health < MAXIMUM
         warrior.rest!
       else
         warrior.walk!
