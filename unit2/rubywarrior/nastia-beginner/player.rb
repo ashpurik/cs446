65a7597f6642b5c6407991e56453157d6ee6fb17
Anastasia Shpurik <ashpurik@mines.edu>
2014-01-21 23:39:50 -0700

    final submission

--- unit2/rubywarrior/nastia-beginner/player.rb
+++ unit2/rubywarrior/nastia-beginner/player.rb
@@ -1,27 +1,55 @@
+#I use instance variables to keep track whether or not
+#the warrior has turned around yet (@turned) and to keep track
+#of the actual warrior (@warrior). 
+
+#I use a class variable to keep track of the warrior's previous health
+
+#I use a constant to keep track of the minimum health my warrior
+#should have to avoid dieing
+
+#I use the rubywarrior array that is returned from warrior.look. 
+#This method returns an array of size 3, with 3 spaces. I go 
+#through each space, checking if it is a captive or an enemy
+
+#I use the hash function when I turn the warrior around, by passing
+#in ":backward" to the pivot! function. :backward is the key in the
+#hash and the value is what causes the warrior to turn in the 
+#correct direction
+
+#I use lots of functions to make the code cleaner in the play_turn function
+
 class Player
 
-  @health = nil
+  @@health = nil
   MINIMUM = 8
-  MAXIMUM = 20
-  @saved = nil
   @turned = nil
   @warrior = nil
 
 
   def play_turn(warrior)
 
+    #this variable keeps track if the warrior has turned around
     @turned = false unless @turned != nil
     @warrior = warrior unless @warrior != nil
+    @@health = warrior.health unless @@health != nil
 
+    #if there is an enemy or captive behind the warrior 
+    #and the warrior hasn't turned around yet, then the
+    #warrior is going to turn around
     if (enemy_behind? || captive_behind?) && @turned == false
       warrior.pivot! :backward
       @turned = true
 
+    #if there is a wall in front of you, turn around
     elsif wall_in_front?
       warrior.pivot! :backward
 
     elsif found_captive?
       warrior.rescue!
+
+    #if you aren't under attack and have low health, rest
+    elsif !under_attack? && warrior.health <= MINIMUM
+      warrior.rest!
 
     elsif enemy_ahead?
       warrior.shoot!
@@ -30,37 +58,40 @@ class Player
       warrior.walk!
       
     end
+
+    #save your previous health
+    @@health = warrior.health
     
   end
   
 
   def enemy_behind?
-    return @warrior.look(:backward).any? { |space| space.enemy? }
+    @warrior.look(:backward).any? { |space| space.enemy? }
   end
 
   def captive_behind?
-    return @warrior.look(:backward).any? { |space| space.captive? }
+    @warrior.look(:backward).any? { |space| space.captive? }
   end
 
   def wall_in_front?
-    return @warrior.feel.wall?
+    @warrior.feel.wall?
   end
 
   def enemy_ahead?
-    return @warrior.look.any? { |space| space.enemy? }
+    @warrior.look.any? { |space| space.enemy? }
   end
 
   def captive_ahead?
-    return @warrior.look.any? { |space| space.captive? }
+    @warrior.look.any? { |space| space.captive? }
   end
 
   def found_captive?
-    return @warrior.feel.captive?
+    @warrior.feel.captive?
   end
 
   def under_attack?
-    return @warrior.health < @health
+    @warrior.health < @@health
   end
 
-end
 
+end
